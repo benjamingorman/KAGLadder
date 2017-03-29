2674c06e06a564f90fe3b01a0bcdb9f1ed241245
Benjamin Gorman <8076bgorman@gmail.com>
2017-03-29 12:23:58 +0100

    Add profile widget script

---
+++ ProfileWidget.py
@@ -0,0 +1,60 @@
+from PIL import Image, ImageDraw, ImageFont
+from flask import Flask, send_file
+
+ELO_TABLE_FILE = "Example_ELO_Table.cfg"
+WIDGET_WIDTH = 280
+WIDGET_HEIGHT = 100
+WIDGET_FONT_HEADER = ImageFont.truetype("FjallaOne-Regular.ttf", 18)
+WIDGET_FONT_BODY = ImageFont.truetype("FjallaOne-Regular.ttf", 14)
+
+
+def get_rating(player_username):
+    rating_archer = "Unrated"
+    rating_builder = "Unrated"
+    rating_knight = "Unrated"
+    with open(ELO_TABLE_FILE, 'r') as f:
+        for line in f:
+            name_with_class, elo = line.split("=")
+            name_with_class = name_with_class.strip()
+            elo = elo.strip()
+
+            username, which_class = name_with_class.split("-")
+            which_class.strip()
+            #print(name_with_class, username, which_class, elo)
+
+            if username == player_username:
+                if which_class == "archer":
+                    rating_archer = elo
+                elif which_class == "builder":
+                    rating_builder = elo
+                elif which_class == "knight":
+                    rating_knight = elo
+
+    return (rating_archer, rating_builder, rating_knight)
+
+
+def create_elo_widget_image(username, file_handle):
+    img = Image.new('RGB', (WIDGET_WIDTH, WIDGET_HEIGHT))
+    draw = ImageDraw.Draw(img)
+    header_color = (255, 0, 0)
+    class_color = (135, 206, 235)
+    body_color = (255, 255, 255)
+
+    (rating_archer, rating_builder, rating_knight) = get_rating(username)
+    draw.text((20, 20), "Rated 1v1 Server - " + username, font=WIDGET_FONT_HEADER, fill=header_color)
+    draw.text((20, 50), "Archer", font=WIDGET_FONT_BODY, fill=class_color)
+    draw.text((20, 70), rating_archer, font=WIDGET_FONT_BODY, fill=body_color)
+    draw.text((80, 50), "Builder", font=WIDGET_FONT_BODY, fill=class_color)
+    draw.text((80, 70), rating_builder, font=WIDGET_FONT_BODY, fill=body_color)
+    draw.text((140, 50), "Knight", font=WIDGET_FONT_BODY, fill=class_color)
+    draw.text((140, 70), rating_knight, font=WIDGET_FONT_BODY, fill=body_color)
+    img.save(file_handle, "PNG")
+
+app = Flask(__name__)
+@app.route("/rating/<username>")
+def get_elo_widget_image(username):
+    print("Getting widget for: " + username)
+    temp = open("tempwidget.png", "w+b")
+    create_elo_widget_image(username, temp)
+    temp.close()
+    return send_file("tempwidget.png", mimetype="image/png")
